f3016d53be8d17883668d7c37398c3829eff1b83
Iionsroar <kiseryota023@gmail.com>
2021-06-15 09:48:15 +0800

    barebones password reset page, layout html & route

--- thesis_grading/user/routes.py
+++ thesis_grading/user/routes.py
@@ -6,3 +6,7 @@ user = Blueprint("user", __name__, url_prefix="/user")
 def login():
     # redir to grading if logged in
     return render_template("user/login.html")
+
+@user.route("/password/reset")
+def reset():
+    return render_template("user/password_reset.html")
