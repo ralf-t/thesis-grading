783ab0dc7dc07652280d0da2499675fe453ed97c
jebjebs <virgiliolopez.jev@gmail.com>
2021-07-14 11:33:24 +0800

    Added user/read page
    Added user/read page

--- thesis_grading/user/routes.py
+++ thesis_grading/user/routes.py
@@ -6,3 +6,46 @@ user = Blueprint("user", __name__, url_prefix="/user")
 def login():
     # redir to grading if logged in
     return render_template("user/login.html")
+
+@user.route("/read")
+def read():
+    #id
+    #username
+    #password
+    #email
+
+    users = [
+        {
+            "id" : "1",
+            "username" : "mahabangmahabangmahabangusername",
+            "password" : "123123123123123123123123123123123213",
+            "email" : "ju@nd3lacruz_averyverylongemailaddresssss@email.com"
+        },
+        {
+            "id" : "1",
+            "username" : "ju@nd3lacruz",
+            "password" : "123",
+            "email" : "ju@nd3lacruz@email.com"
+        },
+        {
+            "id" : "1",
+            "username" : "ju@nd3lacruz",
+            "password" : "123",
+            "email" : "ju@nd3lacruz@email.com"
+        },
+        {
+            "id" : "1",
+            "username" : "ju@nd3lacruz",
+            "password" : "123",
+            "email" : "ju@nd3lacruz@email.com"
+        },
+        {
+            "id" : "1",
+            "username" : "ju@nd3lacruz",
+            "password" : "123",
+            "email" : "ju@nd3lacruz@email.com"
+        }
+
+    ]
+
+    return render_template("user/read.html", users=users)
